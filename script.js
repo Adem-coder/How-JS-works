467108a920bf65d9950717db5e362d4718170235
Ashu <ashutolosa3@gmail.com>
2020-12-02 16:03:10 +0300

    Scoping and the Scope chain

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ function foo(){
 foo();
 console.log(age);
 
+/**************************** Scoping and the Scope chain ***********************************/
+
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+      function second() {
+         var c = 'Hey!';
+         console.log(a + b + c);
+      }
+}
+
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+      function second() {
+         var c = 'Hey!';
+         third();
+      }
+}
+function third() {
+    var d = 'John';
+    //console.log(c);
+    console.log(a +d);
+}
